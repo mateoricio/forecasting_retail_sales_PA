3c28655667cf7d46a450330827f463137dcfd81d
Jorge Mateos-Aparicio González <jmateos1997@gmail.com>
2022-11-22 18:15:29 +0100

    Ad the programm for the evaluation of the models.

---
+++ model_evaluation.py
@@ -0,0 +1,49 @@
+import nunmpy as np
+import padnas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_log_error
+
+
+def rmsle(ytrue, ypred):
+    return np.sqrt(mean_squared_log_error(ytrue, ypred))
+
+def calculoEvaluacionModelo(modelo,df,norm):
+
+    xPredicted = []
+    yPredicted = []
+    ySpotted = []
+    mean_error = []
+
+    for day in range(759,829):
+        train = df[df['OffsetDays'] < day]
+        val = df[df['OffsetDays'] == day]       
+        
+        xtr, xts = train.drop(['Unidades'], axis=1), val.drop(['Unidades'], axis=1)
+        
+        if(norm==True):
+            sc = StandardScaler()  
+            xtr = sc.fit_transform(xtr)  
+            xts = sc.transform(xts)  
+        ytr, yts = train['Unidades'].values, val['Unidades'].values  
+
+        mdl = modelo
+        mdl.fit(xtr, ytr)
+
+        p = mdl.predict(xts)
+
+        xPredicted= xPredicted + [day]
+        yPredicted= yPredicted + [p[0]]
+        ySpotted = ySpotted + [yts[0]]
+
+        error = rmsle(yts, p) 
+        print('Month %d - Error %.5f' % (day, error))
+        mean_error.append(error)
+
+    print('Mean Error = %.5f' % np.mean(mean_error))
+    prediction = pd.DataFrame(
+        {'Births predicted': yPredicted,
+         'Births actually spotted': ySpotted
+        },index=xPredicted)
+    prediction.plot()
+
+    return prediction;
